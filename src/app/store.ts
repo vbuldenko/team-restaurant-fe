325f8a90548d08b376da85eb4f9750d6ca62c864
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-09-11 11:26:15 +0300

    store reducers update

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../features/user/userSlice";
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     user: userReducer,
     // menu: menuReducer,
     // reservations: reservationsReducer,
