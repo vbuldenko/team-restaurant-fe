1b654c6cbb8ff535b4851e84e7ab895b4bc51a5b
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-11-26 14:27:20 +0200

    Hero background grid container

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,12 +5,14 @@ export const Hero = () => {
   const { t } = useTranslation();
   return (
     <div className="hero">
-      <div className="hero__title">
-        <span className="hero__name">{t("home.hero_titles.main")}</span>
-        <span>{t("home.hero_titles.secondary")}</span>
-        <span>{t("home.hero_titles.description")}</span>
+      <div className="hero__content">
+        <div className="hero__text">
+          <span className="hero__name">{t("home.hero_titles.main")}</span>
+          <span>{t("home.hero_titles.secondary")}</span>
+          <span>{t("home.hero_titles.description")}</span>
+        </div>
+        <div className="hero__button">{t("home.hero_button")}</div>
       </div>
-      <div className="hero__button">{t("home.hero_button")}</div>
     </div>
   );
 };
