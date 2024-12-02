ea6de23ecae4a8112e34870e0e5bcce2cb1f3c46
Vladyslav <vladbuldenko@gmail.com>
2024-12-03 00:55:29 +0200

    Menu item partly implemented

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,23 +1,92 @@
 import axios from "axios";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./Menu.scss";
+import classNames from "classnames";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/solid";
 
-interface MenuItem {
+type MenuItemCardProps = {
   id: number;
   name: string;
+  description: string;
   category: string;
   price: number;
-  description: string;
-  available: boolean;
-}
+  image_url: string;
+  selected: boolean;
+  clickHandler: (id: number) => void;
+};
+
+const MenuItemCard: React.FC<MenuItemCardProps> = ({
+  id,
+  name,
+  description,
+  category,
+  price,
+  image_url,
+  selected,
+  calories,
+  weight,
+  likes,
+  clickHandler,
+}) => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count > 1 ? count - 1 : 1);
+  return (
+    <div
+      className={classNames("menu-item", {
+        "menu-item--selected": selected,
+      })}
+      onClick={() => clickHandler(id)}
+    >
+      <div className="menu-item__calories">
+        <span className="w-min">{calories} kcal</span>
+      </div>
+      <div className="menu-item__likes">
+        <span className="w-min">
+          <HeartIcon className="h-5 w-5" />
+          {likes}
+        </span>
+      </div>
+      <div className="menu-item__image-container">
+        <img src={image_url} alt={name} className="menu-item__image" />
+      </div>
+      <div className="menu-item__content">
+        <h3 className="menu-item__name">{name}</h3>
+        <p className="menu-item__description">{description}</p>
+        <div className="flex gap-4 items-center justify-between w-full py-2">
+          <div className="menu-item__counter">
+            <button className="menu-item__counter-button" onClick={decrement}>
+              -
+            </button>
+            <span className="menu-item__counter-value">{count}</span>
+            <button className="menu-item__counter-button" onClick={increment}>
+              +
+            </button>
+          </div>
+          <div className="flex gap-4 items-center">
+            <p className="menu-item__price">${price.toFixed(2)}</p>
+            <button className="menu-item__order-button">
+              <ShoppingCartIcon className="h-5 w-5" />
+              {selected && "Add to order"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Menu = () => {
   const { t } = useTranslation();
-  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+  const [menuItems, setMenuItems] = useState<MenuItemCardProps[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
 
   useEffect(() => {
     // Adjust the path based on where your JSON is located
@@ -33,22 +102,25 @@ const Menu = () => {
       });
   }, []);
 
+  const handleItemClick = (id: number) => {
+    setSelectedItemId(id);
+  };
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
-    <div>
+    <div className="container py-20">
       <h1>Restaurant Menu</h1>
-      <ul>
+      <div className="menu">
         {menuItems.map((item) => (
-          <li key={item.id}>
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-            <p>Category: {item.category}</p>
-            <p>Price: ${item.price.toFixed(2)}</p>
-          </li>
+          <MenuItemCard
+            key={item.id}
+            {...item}
+            selected={item.id === selectedItemId}
+            clickHandler={handleItemClick}
+          />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
