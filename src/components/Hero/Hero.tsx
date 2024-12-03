561fa87afb20b18602c7e3689e8f3275fd3da6bc
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-12-03 16:08:05 +0200

    Small update in header and hero components

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,17 +6,9 @@ export const Hero = () => {
   return (
     <div className="hero">
       <div className="hero__content">
-        {/* <div className="hero__text">
-          <span>{t("home.hero_titles.main")}</span>
-          <span>{t("home.hero_titles.secondary")}</span>
-          <span>{t("home.hero_titles.description")}</span>
-        </div> */}
         <div className="hero__text">
           <span>{t("home.hero_titles.main")}</span>
-          <span>
-            {t("home.hero_titles.secondary")}{" "}
-            {t("home.hero_titles.description")}
-          </span>
+          <span>{t("home.hero_titles.description")}</span>
         </div>
         <div className="hero__button">{t("home.hero_button")}</div>
       </div>
