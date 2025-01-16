e97ddcc0c53fe0f74e69aa32c50a187d0406c963
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2025-01-16 15:24:50 +0200

    FE: userSlice update

--- src/app/features/user/userSlice.ts
+++ src/app/features/user/userSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../../store";
 import { User } from "../../../types/User";
 import { ErrorResponse } from "../../../types/Error";
+import { activate, checkAuth, login, logOut } from "./userThunk";
 
 interface UserState {
   loading: boolean;
@@ -9,6 +10,7 @@ interface UserState {
   isAuthenticated: boolean;
   error: string | null;
 }
+
 // Define the initial state using that type
 const initialState: UserState = {
   loading: false,
@@ -24,10 +26,10 @@ const handlePending = (state: UserState) => {
 
 const handleFulfilled = (
   state: UserState,
-  action: PayloadAction<Partial<User>>
+  action: PayloadAction<Partial<User> | undefined>
 ) => {
   state.isAuthenticated = true;
-  state.data = action.payload;
+  state.data = action.payload || null;
   state.loading = false;
   state.error = null;
 };
@@ -37,6 +39,7 @@ const handleRejected = (
   action: PayloadAction<ErrorResponse | undefined>
 ) => {
   state.isAuthenticated = false;
+  state.data = null;
   state.loading = false;
   state.error = action.payload?.message || "An unknown error occurred";
 };
@@ -45,14 +48,18 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setError: (state, action) => {
+    setError: (state, action: PayloadAction<string>) => {
       state.error = action.payload;
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(checkAuth.pending, handlePending)
-      .addCase(checkAuth.fulfilled, handleFulfilled)
+      .addCase(checkAuth.fulfilled, (state) => {
+        state.isAuthenticated = true;
+        state.loading = false;
+        state.error = null;
+      })
       .addCase(checkAuth.rejected, handleRejected)
       .addCase(login.pending, handlePending)
       .addCase(login.fulfilled, handleFulfilled)
@@ -63,6 +70,7 @@ export const userSlice = createSlice({
       .addCase(logOut.fulfilled, (state) => {
         state.isAuthenticated = false;
         state.loading = false;
+        state.data = null;
         state.error = null;
       })
       .addCase(logOut.rejected, handleRejected);
--- src/app/features/user/userThunk.ts
+++ src/app/features/user/userThunk.ts
@@ -1,14 +1,66 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { authService } from "../../../services/authService";
+import { accessTokenService } from "../../../services/accessTokenService";
 import { userService } from "../../../services/userService";
+import { ErrorResponse } from "../../../types/Error";
+import { getErrorMessage } from "../../../utils";
+import { setError } from "./userSlice";
+import { User } from "../../../types/User";
 
-export const fetchUserData = createAsyncThunk(
-  "user/fetchUser",
-  async (_, { rejectWithValue }) => {
-    try {
-      const user = await userService.getProfile();
-      return user;
-    } catch (error: any) {
-      return rejectWithValue(error.response.data.message);
-    }
+export const checkAuth = createAsyncThunk<
+  void, // Return type of the successful request
+  void, // Argument type
+  { rejectValue: ErrorResponse } // Type for rejected value
+>("user/checkAuth", async (_, { rejectWithValue }) => {
+  try {
+    const { token } = await authService.refresh();
+    accessTokenService.save(token);
+  } catch (error: any) {
+    accessTokenService.remove();
+    return rejectWithValue({ message: getErrorMessage(error) });
   }
-);
+});
+
+export const login = createAsyncThunk<
+  User,
+  { email: string; password: string },
+  { rejectValue: ErrorResponse }
+>("user/login", async (credentials, { dispatch, rejectWithValue }) => {
+  try {
+    const { token } = await authService.login(credentials);
+    accessTokenService.save(token);
+    return await userService.getProfile();
+  } catch (error: any) {
+    setTimeout(() => dispatch(setError(null)), 2000);
+    return rejectWithValue({ message: getErrorMessage(error) });
+  }
+});
+
+export const activate = createAsyncThunk<
+  User,
+  string,
+  { rejectValue: ErrorResponse }
+>("user/activation", async (activationToken, { dispatch, rejectWithValue }) => {
+  try {
+    const { token } = await authService.activate(activationToken);
+    accessTokenService.save(token);
+    return await userService.getProfile();
+  } catch (error: any) {
+    setTimeout(() => dispatch(setError(null)), 2000);
+    return rejectWithValue({ message: getErrorMessage(error) });
+  }
+});
+
+export const logOut = createAsyncThunk<
+  void,
+  void,
+  { rejectValue: ErrorResponse }
+>("auth/logout", async (_, { dispatch, rejectWithValue }) => {
+  try {
+    accessTokenService.remove();
+    await authService.logout();
+  } catch (error: any) {
+    setTimeout(() => dispatch(setError(null)), 3000);
+    return rejectWithValue({ message: getErrorMessage(error) });
+  }
+});
